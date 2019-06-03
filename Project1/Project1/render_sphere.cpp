879b51f017f0a41e9f2b5bfb371f63e97f2c5abf
Siusarna <42269105+Siusarna@users.noreply.github.com>
2019-06-04 01:44:51 +0300

    Add algorithm Mollera-Trumbora

--- Project1/Project1/render_sphere.cpp
+++ Project1/Project1/render_sphere.cpp
@@ -4,10 +4,41 @@
 #include <cmath>
 #include <math.h> 
 
-vec3 cast_ray(const vec3& orig, const vec3& dir, const Sphere& sphere) {
-	float sphere_dist = std::numeric_limits<float>::max();
-	if (!sphere.ray_intersect(orig, dir, sphere_dist)) {
-		return vec3(0.2, 0.7, 0.8); // background color
+//vec3 cast_ray(const vec3& orig, const vec3& dir, const Sphere& sphere) {
+//	float sphere_dist = std::numeric_limits<float>::max();
+//	if (!sphere.ray_intersect(orig, dir, sphere_dist)) {
+//		return vec3(0.2, 0.7, 0.8); // background color
+//	}
+//	return vec3(0.4, 0.4, 0.3);
+//}
+
+vec3 triangle_intersection(const vec3& orig,
+	const vec3& dir,
+	const vec3& v0,
+	const vec3& v1,
+	const vec3& v2) {
+	vec3 e1 = v1 - v0;
+	vec3 e2 = v2 - v0;
+	// Вычисление вектора нормали к плоскости
+	vec3 pvec = cross(dir, e2);
+	float det = dot(e1, pvec);
+
+	// Луч параллелен плоскости
+	if (det < 1e-8 && det > -1e-8) {
+		return vec3(0.2, 0.7, 0.8);
+	}
+
+	float inv_det = 1 / det;
+	vec3 tvec = orig - v0;
+	float u = dot(tvec, pvec) * inv_det;
+	if (u < 0 || u > 1) {
+		return vec3(0.2, 0.7, 0.8);
+	}
+
+	vec3 qvec = cross(tvec, e1);
+	float v = dot(dir, qvec) * inv_det;
+	if (v < 0 || u + v > 1) {
+		return vec3(0.2, 0.7, 0.8);
 	}
 	return vec3(0.4, 0.4, 0.3);
 }
@@ -23,7 +54,7 @@ void render(const Sphere &sphere) {
 			float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width / (float)height;
 			float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
 			vec3 dir = glm::normalize(vec3(x, y, -1));
-			framebuffer[j][i] = cast_ray(vec3(0, 0, 0), dir, sphere);
+			framebuffer[j][i] = triangle_intersection(vec3(0, 0,-5), dir, vec3(0,0,0), vec3(-1,1,0),vec3(2, 1, 0));
 		}
 	}
 	image im;
